ea7b7ec73555a941c52e83e0961fb74beaf7b5f6
zeyad tarek <vetogamer5@outlook.com>
2026-03-19 03:06:39 +0200

    Q2

--- Program.cs
+++ Program.cs
@@ -11,12 +11,28 @@ namespace ConsoleApp1
             //A generic class is a class that works with any data type using a type parameter(e.g., <T>).
 
             //Why use generics?
-            
+
             //Code reusability
-            
+
             //Type safety(no casting errors)
-            
+
             //Better performance
+            #endregion
+            #region Q2
+            //    class Container<T>
+            //{
+            //    private T item;
+
+            //    public void Add(T value)
+            //    {
+            //        item = value;
+            //    }
+
+            //    public T Get()
+            //    {
+            //        return item;
+            //    }
+            //} 
             #endregion
         }
     }
