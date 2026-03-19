728647d3bd22e7f78a9b61a84027101294110d4f
zeyad tarek <vetogamer5@outlook.com>
2026-03-19 03:06:07 +0200

    Q1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ConsoleApp1
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //A generic class is a class that works with any data type using a type parameter(e.g., <T>).
+
+            //Why use generics?
+            
+            //Code reusability
+            
+            //Type safety(no casting errors)
+            
+            //Better performance
+            #endregion
         }
     }
 }
